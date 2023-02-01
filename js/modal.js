13eda0e8067698fcfea1dd64b6752551f1a30fbf
DmytroTrn <115163181+DmytroTrn@users.noreply.github.com>
2023-02-01 16:16:18 +0100

    page ready for adaptive
    01.02.2023

--- js/modal.js
+++ js/modal.js
@@ -3,36 +3,35 @@ let close_modal = document.getElementById('close_modal');
 let modal = document.getElementById('modal');
 let body = document.getElementsByTagName('body')[0];
 for (let i = 0; i < open_modal.length; i++) {
-    open_modal[i].onclick = function() { 
-        modal.classList.add('modal_vis'); 
-        modal.classList.remove('bounceOutDown'); 
-        body.classList.add('body_block'); 
+    open_modal[i].onclick = function() {
+        modal.classList.add('modal_vis');
+        modal.classList.remove('bounceOutDown');
+        body.classList.add('body_block');
     };
 }
-close_modal.onclick = function() { 
-    modal.classList.add('bounceOutDown'); 
-    window.setTimeout(function() { 
+close_modal.onclick = function() {
+    modal.classList.add('bounceOutDown');
+    window.setTimeout(function() {
         modal.classList.remove('modal_vis');
-        body.classList.remove('body_block'); 
+        body.classList.remove('body_block');
     }, 500);
 };
 
-var slideIndex = 0;
-                showSlides();
-
-                function showSlides() {
-                    var i;
-                    var slides = document.getElementsByClassName("mySlides");
-                    var dots = document.getElementsByClassName("dot");
-                    for (i = 0; i < slides.length; i++) {
-                        slides[i].style.display = "none";
-                    }
-                    slideIndex++;
-                    if (slideIndex > slides.length) { slideIndex = 1 }
-                    for (i = 0; i < dots.length; i++) {
-                        dots[i].className = dots[i].className.replace(" active", "");
-                    }
-                    slides[slideIndex - 1].style.display = "block";
-                    dots[slideIndex - 1].className += " active";
-                    setTimeout(showSlides, 4000); // Change image every 2 seconds
-                }
+// let open_modal = document.querySelectorAll('.open_modal');
+// let close_modal = document.getElementById('close_modal');
+// let modal = document.getElementById('modal');
+// let body = document.getElementsByTagName('body')[0];
+// for (let i = 0; i < open_modal.length; i++) {
+//     open_modal[i].onclick = function() { // клик на открытие
+//         modal.classList.add('modal_vis'); // добавляем видимость окна
+//         modal.classList.remove('bounceOutDown'); // удаляем эффект закрытия
+//         body.classList.add('body_block'); // убираем прокрутку
+//     };
+// }
+// close_modal.onclick = function() { // клик на закрытие
+//     modal.classList.add('bounceOutDown'); // добавляем эффект закрытия
+//     window.setTimeout(function() { // удаляем окно через полсекунды (чтобы увидеть эффект закрытия).
+//         modal.classList.remove('modal_vis');
+//         body.classList.remove('body_block'); // возвращаем прокрутку
+//     }, 500);
+// };
